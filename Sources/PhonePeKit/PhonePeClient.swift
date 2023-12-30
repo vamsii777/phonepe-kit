87590361111eda186c55a6701b878dabb1c5ad88
Vamsi Madduluri <vamsi@dewonderstruck.com>
2023-12-30 14:46:04 +0530

    Refactor PhonePeClient initialization and add payments functionality

--- Sources/PhonePeKit/PhonePeClient.swift
+++ Sources/PhonePeKit/PhonePeClient.swift
@@ -9,20 +9,17 @@ import NIO
 import AsyncHTTPClient
 
 public final class PhonePeClient {
-    // MARK: - Core Resources
+
     public var subscriptions: PhonePeSubscriptionRoutes
-
-    // MARK: - Additional Functionalities
-    // ... other functionalities like balances, users, etc.
-
+    public var payments: PhonePePayRoutes
+    
     var handler: PhonePeAPIHandler
 
-    // Assuming Environment is a publicly accessible enum
     public init(httpClient: HTTPClient, saltKey: String, saltIndex: String, environment: Environment) {
         handler = PhonePeAPIHandler(httpClient: httpClient, saltKey: saltKey, saltIndex: saltIndex, environment: environment)
-
-        // Instantiate PhonePeSubscriptionRoutes instead of SubscriptionRoutes
+    
         subscriptions = PhonePeSubscriptionRoutes(apiHandler: handler, baseUrl: environment.baseUrl)
-        // ... initialize other routes
+        payments = PhonePePayRoutes(apiHandler: handler, baseUrl: environment.baseUrl)
+        
     }
 }
