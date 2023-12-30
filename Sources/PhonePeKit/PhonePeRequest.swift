d1dfa23e4d3b27764f323e7059b103aa7a698494
Vamsi Madduluri <vamsi@dewonderstruck.com>
2023-12-30 14:46:14 +0530

    Remove unused imports and refactor signature generation in PhonePeRequest.swift

--- Sources/PhonePeKit/PhonePeRequest.swift
+++ Sources/PhonePeKit/PhonePeRequest.swift
@@ -6,7 +6,6 @@
 //
 
 import Foundation
-import Crypto
 import NIO
 import NIOFoundationCompat
 import NIOHTTP1
@@ -65,7 +64,7 @@ struct PhonePeAPIHandler {
                                      "Accept": "application/json"]
         headers.forEach { _headers.replaceOrAdd(name: $0.name, value: $0.value) }
         
-        let signature = try await generateSignature(path: path, body: body)
+        let signature = try await Request.generateSignature(path: path, body: body, saltKey: saltKey, saltIndex: saltIndex)
         _headers.add(name: "X-VERIFY", value: signature)
         
         var request = HTTPClientRequest(url: "\(environment.baseUrl)\(path)?\(query)")
@@ -75,58 +74,9 @@ struct PhonePeAPIHandler {
         
         let response = try await httpClient.execute(request, timeout: .seconds(60))
         let responseData = try await response.body.collect(upTo: 1024 * 1024 * 100) // 100 MB limit
-        guard response.status == .ok else {
-            let errorDescription = String(buffer: responseData)
-            let statusCode = response.status.code
-            let userInfo: [String: Any] = [
-                NSLocalizedDescriptionKey: errorDescription,
-                "StatusCode": statusCode
-            ]
-            throw NSError(domain: "PhonePeAPIError", code: Int(statusCode), userInfo: userInfo)
-        }
-        
         
         return try decoder.decode(T.self, from: Data(buffer: responseData))
     }
     
-    private func generateSignature(path: String, body: HTTPClientRequest.Body) async throws -> String {
-        let bodyDataString = try await extractBodyString(body: body)
-        let base64EncodedRequest = try parseBase64StringFromJSON(jsonString: bodyDataString)
-        let combinedString = base64EncodedRequest + path + saltKey
-        return calculateSHA256(inputString: combinedString) + "###" + saltIndex
-    }
     
-    private func extractBodyString(body: HTTPClientRequest.Body) async throws -> String {
-        var bodyData = Data()
-        for try await buffer in body {
-            bodyData.append(contentsOf: buffer.readableBytesView)
-        }
-        guard let bodyString = String(data: bodyData, encoding: .utf8) else {
-            // TODO: Use PhonePeError instead of SubscriptionError
-            throw SubscriptionError.bodyExtractionFailed
-        }
-        return bodyString
-    }
-    
-    private func parseBase64StringFromJSON(jsonString: String) throws -> String {
-        guard let jsonData = jsonString.data(using: .utf8),
-              let jsonObject = try? JSONDecoder().decode([String: String].self, from: jsonData),
-              let base64EncodedRequest = jsonObject["request"] else {
-            // TODO: Use PhonePeError instead of SubscriptionError
-            throw SubscriptionError.jsonDecodingFailed
-        }
-        return base64EncodedRequest
-    }
-    
-    private func calculateSHA256(inputString: String) -> String {
-        let inputData = Data(inputString.utf8)
-        let hashed = SHA256.hash(data: inputData)
-        return hashed.compactMap { String(format: "%02x", $0) }.joined()
-    }
-}
-
-
-enum SignatureGenerationError: Error {
-    case invalidBodyFormat
-    case dataExtractionFailed
 }
